58a728df9c0faaec4c0610e36f96687d44198758
Fumiaki Nakao <imakeanewstory@gmail.com>
2018-02-06 01:12:52 +0900

    新規投稿ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'posts/index'=>"posts#index"
 
+
+  get 'posts/index'=>"posts#index"
+  get 'posts/new'=>"posts#new"
+  get 'posts/:id'=>"posts#show"
   get '/'=>"home#top"
   get 'about'=>"home#about"
+
+  post "posts/create"=>"posts#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
