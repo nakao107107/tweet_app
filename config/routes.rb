d164523ca06f7b5f4d75ba3726c68d04800b91e6
Fumiaki Nakao <imakeanewstory@gmail.com>
2018-02-07 11:37:02 +0900

    投稿の編集、削除機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   get 'about'=>"home#about"
 
   post "posts/create"=>"posts#create"
+  get "posts/:id/edit"=>"posts#edit"
+  post "posts/:id/update"=>"posts#update"
+
+  post "posts/:id/destroy"=>"posts#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
